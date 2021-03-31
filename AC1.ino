0e5130c004009a1706728db933aafea1cbca43b3
MuriloCardoso <79269880+Murilo326@users.noreply.github.com>
2021-03-31 12:58:20 -0300

    Update AC1.ino

--- AC1.ino
+++ AC1.ino
@@ -2,3 +2,11 @@
 const int vermelho = 5;
 const int verde = 6;
 const int azul = 7;
+bool estadoLedVermelho = false;
+
+//acréscimo de um botão
+const int botao1 = 2;
+const int botao2 = 2;
+unsigned long lastDebounceTime1 = 0;
+unsigned long lastDebounceTime2 = 0;
+const int botaoDelay = 100;
