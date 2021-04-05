61d4fde984ac1e97e071c77a7b5325aa5dc380f1
Gabriel Santoro <79268036+santoroo@users.noreply.github.com>
2021-04-04 22:17:02 -0300

    ajuste final do código

--- AC1.ino
+++ AC1.ino
@@ -2,25 +2,27 @@
 const int vermelho = 5;
 const int verde = 6;
 const int azul = 7;
+
 bool estadoLedVermelho = false;
 
 //acréscimo de um botão
 const int botao1 = 2;
-const int botao2 = 2;
+const int botao2 = 3;
 unsigned long lastDebounceTime1 = 0;
 unsigned long lastDebounceTime2 = 0;
 const int botaoDelay = 100;
+
 void setup()
 {
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
-//saída do led azul
+//saída do led azul  
   pinMode(vermelho, OUTPUT);
   pinMode(verde, OUTPUT);
   pinMode(azul, OUTPUT);
-   
+  
   Serial.begin(9600);
-//nome do nosso grupo
+//nome do nosso grupo	
   Serial.println("AC1 - Meu Primeiro Projeto 2021");
   Serial.println("                           V1.0");
   Serial.println("Grupo:MLE");
@@ -29,35 +31,35 @@ void setup()
 void loop()
 {
   if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao1)){
-      Serial.println("botao 1 apertado");
+  	Serial.println("botao 1 apertado");
     ledVermelho(true);
-      lastDebounceTime1 = millis();
+  	lastDebounceTime1 = millis();
   }
-  if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao2)){
-      Serial.println("botao 2 apertado");
+  if((millis() - lastDebounceTime2) > botaoDelay && digitalRead(botao2)){
+  	Serial.println("botao 2 apertado");
    ledVermelho(false);
-      lastDebounceTime2 = millis();
+  	lastDebounceTime2 = millis();
   }
- 
   //verificador de tempetura
-  if(getTemperatura() > 30){
+  if(getTemperatura() > 15){
     ledAzul(true);
     Serial.println ("Temperatura acima da temperatura ideal");
   }else{
-      ledAzul(false); 
-  Serial.println ("Temperatura ok");
- }
-  //alteração na luminosidade
-    if(getLuminosidade() > 15) {
-      ledVerde (true); 
-      Serial.println ("Luminosidade acima do ideal");
-      }else{
-      ledVerde (false); 
-       Serial.println ("Luminosidade ok");
-    }
-
+  	ledAzul(false); 
+  	Serial.println ("Temperatura ok");
+  }
+//alteração na luminosidade
+  if(getLuminosidade() > 5){
+    ledVerde(true); 
+    Serial.println ("Luminosidade acima do ideal");
+  }else{
+    ledVerde(false); 
+    Serial.println ("Luminosidade ok");
+  }  
+      
   delay(10);
 }
+
 void ledVermelho(bool estado){
  digitalWrite(vermelho,estado);
 }
@@ -66,17 +68,17 @@ void ledVerde(bool estado){
  
 }
 void ledAzul(bool estado){
-    digitalWrite(azul,estado);
+	digitalWrite(azul,estado);
 }
 //temperatura
 int getTemperatura(){
-      int temperaturaC;
-    temperaturaC = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
-      return temperaturaC;
-}
+  	int temperaturaC;
+	temperaturaC = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
+  	return temperaturaC;
+} 
 //funcao de leitura da luminosidade
 int getLuminosidade(){
-      int luminosidade;
-    luminosidade = map(analogRead(A1), 6, 619, -3, 10);
-      return luminosidade;
+  	int luminosidade;
+  	luminosidade = map(analogRead(A1), 6, 619, -3, 10);
+  	return luminosidade;
 }
